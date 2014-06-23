21805612e7437b4fe2dbd7a4c2fe884688247a23
Alex Gotev <alexbbb@users.noreply.github.com>
2014-06-23 12:27:32 +0200

    Replaced tabs with spaces

--- src/com/alexbbb/uploadservice/UploadService.java
+++ src/com/alexbbb/uploadservice/UploadService.java
@@ -341,10 +341,10 @@ public class UploadService extends IntentService {
             filteredMessage = responseMessage;
         }
 
-		if (responseCode >= 200 && responseCode <= 299)
-			updateNotificationCompleted();
-		else
-			updateNotificationError();
+        if (responseCode >= 200 && responseCode <= 299)
+            updateNotificationCompleted();
+        else
+            updateNotificationError();
 
         final Intent intent = new Intent(BROADCAST_ACTION);
         intent.putExtra(UPLOAD_ID, uploadId);
