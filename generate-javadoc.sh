49dbf71bb172f564a0c20eabbbeb5957cfcba42b
Alex Gotev <alex@gotev.net>
2015-12-04 06:29:12 +0100

    automate javadoc publishing

--- generate-javadoc.sh
+++ generate-javadoc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
-./gradlew clean
-./gradlew javadoc
+./gradlew clean javadoc
 rm -rf ../javadoc
 mv uploadservice/build/docs/javadoc/ ../
 git checkout gh-pages
 rm -rf javadoc
 mv ../javadoc .
+cd javadoc
+git add . --force
+git commit -m "updated javadocs"
+git push
+cd ..
+git checkout master
