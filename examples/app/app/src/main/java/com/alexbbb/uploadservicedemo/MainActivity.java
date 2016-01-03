95eba8ffe20523972e321291d5597329abbe2e1a
Mahmoud Abdurrahman <m.abdurrahman@startappz.com>
2016-01-04 01:39:36 +0200

    modified the demo app according to the parallel multiple upload support

--- examples/app/app/src/main/java/com/alexbbb/uploadservicedemo/MainActivity.java
+++ examples/app/app/src/main/java/com/alexbbb/uploadservicedemo/MainActivity.java
@@ -1,15 +1,21 @@
 package com.alexbbb.uploadservicedemo;
 
 import android.app.Activity;
+import android.content.ClipData;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alexbbb.uploadservice.BinaryUploadRequest;
@@ -24,6 +30,10 @@ import com.nononsenseapps.filepicker.FilePickerActivity;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import butterknife.Bind;
@@ -42,39 +52,52 @@ public class MainActivity extends AppCompatActivity {
     private static final String USER_AGENT = "UploadServiceDemo/" + BuildConfig.VERSION_NAME;
     private static final int FILE_CODE = 1;
 
-    @Bind(R.id.uploadProgress) ProgressBar progressBar;
+    @Bind(R.id.container) ViewGroup container;
     @Bind(R.id.multipartUploadButton) Button multipartUploadButton;
     @Bind(R.id.binaryUploadButton) Button binaryUploadButton;
-    @Bind(R.id.cancelUploadButton) Button cancelUploadButton;
+    @Bind(R.id.cancelAllUploadsButton) Button cancelAllUploadsButton;
     @Bind(R.id.serverURL) EditText serverUrl;
-    @Bind(R.id.fileToUpload) EditText fileToUpload;
+    @Bind(R.id.filesToUpload) EditText filesToUpload;
     @Bind(R.id.parameterName) EditText parameterName;
     @Bind(R.id.displayNotification) CheckBox displayNotification;
+
+    private Map<String, UploadProgressViewHolder> uploadProgressHolders = new HashMap<>();
 
     private final UploadServiceBroadcastReceiver uploadReceiver =
             new UploadServiceBroadcastReceiver() {
 
         @Override
         public void onProgress(String uploadId, int progress) {
-            progressBar.setProgress(progress);
-
             Log.i(TAG, "The progress of the upload with ID " + uploadId + " is: " + progress);
+
+            if (uploadProgressHolders.get(uploadId) == null)
+                return;
+
+            uploadProgressHolders.get(uploadId).progressBar.setProgress(progress);
         }
 
         @Override
         public void onError(String uploadId, Exception exception) {
-            progressBar.setProgress(0);
-
             Log.e(TAG, "Error in upload with ID: " + uploadId + ". "
                         + exception.getLocalizedMessage(), exception);
+
+            if (uploadProgressHolders.get(uploadId) == null)
+                return;
+
+            container.removeView(uploadProgressHolders.get(uploadId).itemView);
+            uploadProgressHolders.remove(uploadId);
         }
 
         @Override
         public void onCompleted(String uploadId, int serverResponseCode, String serverResponseMessage) {
-            progressBar.setProgress(0);
-
             Log.i(TAG, "Upload with ID " + uploadId + " is completed: " + serverResponseCode + ", "
                        + serverResponseMessage);
+
+            if (uploadProgressHolders.get(uploadId) == null)
+                return;
+
+            container.removeView(uploadProgressHolders.get(uploadId).itemView);
+            uploadProgressHolders.remove(uploadId);
         }
     };
 
@@ -83,9 +106,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        progressBar.setMax(100);
-        progressBar.setProgress(0);
 
         // Uncomment this line to enable self-signed SSL certificates in HTTPS connections
         // WARNING: Do not use in production environment. Recommended for development only
@@ -108,12 +128,12 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
-    private UploadNotificationConfig getNotificationConfig() {
+    private UploadNotificationConfig getNotificationConfig(String filename) {
         if (!displayNotification.isChecked()) return null;
 
         return new UploadNotificationConfig()
             .setIcon(R.drawable.ic_upload)
-            .setTitle(getString(R.string.app_name))
+            .setTitle(filename)
             .setInProgressMessage(getString(R.string.uploading))
             .setCompletedMessage(getString(R.string.upload_success))
             .setErrorMessage(getString(R.string.upload_error))
@@ -126,56 +146,80 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.multipartUploadButton)
     void onMultipartUploadClick() {
         final String serverUrlString = serverUrl.getText().toString();
-        final String fileToUploadPath = fileToUpload.getText().toString();
         final String paramNameString = parameterName.getText().toString();
-        final String uploadID = UUID.randomUUID().toString();
 
-        try {
-            new MultipartUploadRequest(this, uploadID, serverUrlString)
-                .addFileToUpload(fileToUploadPath, paramNameString)
-                .setNotificationConfig(getNotificationConfig())
-                .setCustomUserAgent(USER_AGENT)
-                .setMaxRetries(2)
-                .startUpload();
+        final String filesToUploadString = filesToUpload.getText().toString();
+        final String[] filesToUploadArray = filesToUploadString.split(",");
 
-        // these are the different exceptions that may be thrown
-        } catch (FileNotFoundException exc) {
-            showToast(exc.getMessage());
-        } catch (IllegalArgumentException exc) {
-            showToast("Missing some arguments. " + exc.getMessage());
-        } catch (MalformedURLException exc) {
-            showToast(exc.getMessage());
+        for (String fileToUploadPath : filesToUploadArray) {
+            try {
+                final String uploadID = UUID.randomUUID().toString();
+                final String filename = getFilename(fileToUploadPath);
+
+                new MultipartUploadRequest(this, uploadID, serverUrlString)
+                        .addFileToUpload(fileToUploadPath, paramNameString)
+                        .setNotificationConfig(getNotificationConfig(filename))
+                        .setCustomUserAgent(USER_AGENT)
+                        .setMaxRetries(2)
+                        .startUpload();
+
+                View uploadProgressView = getLayoutInflater().inflate(R.layout.view_upload_progress, null);
+                UploadProgressViewHolder viewHolder = new UploadProgressViewHolder(uploadProgressView, filename);
+                viewHolder.uploadId = uploadID;
+                container.addView(viewHolder.itemView, 0);
+                uploadProgressHolders.put(uploadID, viewHolder);
+
+                // these are the different exceptions that may be thrown
+            } catch (FileNotFoundException exc) {
+                showToast(exc.getMessage());
+            } catch (IllegalArgumentException exc) {
+                showToast("Missing some arguments. " + exc.getMessage());
+            } catch (MalformedURLException exc) {
+                showToast(exc.getMessage());
+            }
         }
     }
 
     @OnClick(R.id.binaryUploadButton)
     void onUploadBinaryClick() {
         final String serverUrlString = serverUrl.getText().toString();
-        final String fileToUploadPath = fileToUpload.getText().toString();
-        final String uploadID = UUID.randomUUID().toString();
 
-        try {
-            new BinaryUploadRequest(this, uploadID, serverUrlString)
-                .addHeader("file-name", new File(fileToUploadPath).getName())
-                .setFileToUpload(fileToUploadPath)
-                .setNotificationConfig(getNotificationConfig())
-                .setCustomUserAgent(USER_AGENT)
-                .setMaxRetries(2)
-                .startUpload();
+        final String filesToUploadString = filesToUpload.getText().toString();
+        final String[] filesToUploadArray = filesToUploadString.split(",");
 
-        // these are the different exceptions that may be thrown
-        } catch (FileNotFoundException exc) {
-            showToast(exc.getMessage());
-        } catch (IllegalArgumentException exc) {
-            showToast("Missing some arguments. " + exc.getMessage());
-        } catch (MalformedURLException exc) {
-            showToast(exc.getMessage());
+        for (String fileToUploadPath : filesToUploadArray) {
+            try {
+                final String uploadID = UUID.randomUUID().toString();
+                final String filename = getFilename(fileToUploadPath);
+
+                new BinaryUploadRequest(this, uploadID, serverUrlString)
+                        .addHeader("file-name", new File(fileToUploadPath).getName())
+                        .setFileToUpload(fileToUploadPath)
+                        .setNotificationConfig(getNotificationConfig(filename))
+                        .setCustomUserAgent(USER_AGENT)
+                        .setMaxRetries(2)
+                        .startUpload();
+
+                View uploadProgressView = getLayoutInflater().inflate(R.layout.view_upload_progress, null);
+                UploadProgressViewHolder viewHolder = new UploadProgressViewHolder(uploadProgressView, filename);
+                viewHolder.uploadId = uploadID;
+                container.addView(viewHolder.itemView, 0);
+                uploadProgressHolders.put(uploadID, viewHolder);
+
+                // these are the different exceptions that may be thrown
+            } catch (FileNotFoundException exc) {
+                showToast(exc.getMessage());
+            } catch (IllegalArgumentException exc) {
+                showToast("Missing some arguments. " + exc.getMessage());
+            } catch (MalformedURLException exc) {
+                showToast(exc.getMessage());
+            }
         }
     }
 
-    @OnClick(R.id.cancelUploadButton)
-    void onCancelUploadButtonClick() {
-        UploadService.stopCurrentUpload();
+    @OnClick(R.id.cancelAllUploadsButton)
+    void onCancelAllUploadsButtonClick() {
+        UploadService.stopAllUploads();
     }
 
     @OnClick(R.id.pickFile)
@@ -183,7 +227,7 @@ public class MainActivity extends AppCompatActivity {
         // Starts NoNonsense-FilePicker (https://github.com/spacecowboy/NoNonsense-FilePicker)
         Intent intent = new Intent(this, FilePickerActivity.class);
 
-        intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false);
+        intent.putExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, true);
         intent.putExtra(FilePickerActivity.EXTRA_ALLOW_CREATE_DIR, false);
         intent.putExtra(FilePickerActivity.EXTRA_MODE, FilePickerActivity.MODE_FILE);
 
@@ -200,8 +244,77 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == FILE_CODE && resultCode == Activity.RESULT_OK) {
-            String absolutePath = new File(data.getData().getPath()).getAbsolutePath();
-            fileToUpload.setText(absolutePath);
+            List<Uri> resultUris = new ArrayList<>();
+
+            if (data.getBooleanExtra(FilePickerActivity.EXTRA_ALLOW_MULTIPLE, false)) {
+                // For JellyBean and above
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    ClipData clip = data.getClipData();
+
+                    if (clip != null) {
+                        for (int i = 0; i < clip.getItemCount(); i++) {
+                            resultUris.add(clip.getItemAt(i).getUri());
+                        }
+                    }
+                    // For Ice Cream Sandwich
+                } else {
+                    ArrayList<String> paths = data.getStringArrayListExtra(FilePickerActivity.EXTRA_PATHS);
+
+                    if (paths != null) {
+                        for (String path: paths) {
+                            resultUris.add(Uri.parse(path));
+                        }
+                    }
+                }
+            } else {
+                resultUris.add(data.getData());
+            }
+
+            StringBuilder absolutePathsConcat = new StringBuilder();
+            for (Uri uri : resultUris) {
+                if (absolutePathsConcat.length() == 0) {
+                    absolutePathsConcat.append(new File(uri.getPath()).getAbsolutePath());
+                } else {
+                    absolutePathsConcat.append(",").append(new File(uri.getPath()).getAbsolutePath());
+                }
+            }
+            filesToUpload.setText(absolutePathsConcat.toString());
+        }
+    }
+
+    private String getFilename(String filepath) {
+        if (filepath == null)
+            return null;
+
+        final String[] filepathParts = filepath.split("/");
+
+        return filepathParts[filepathParts.length - 1];
+    }
+
+    class UploadProgressViewHolder {
+        View itemView;
+
+        @Bind(R.id.uploadTitle) TextView uploadTitle;
+        @Bind(R.id.uploadProgress) ProgressBar progressBar;
+
+        String uploadId;
+
+        UploadProgressViewHolder(View view, String filename) {
+            itemView = view;
+            ButterKnife.bind(this, itemView);
+
+            progressBar.setMax(100);
+            progressBar.setProgress(0);
+
+            uploadTitle.setText(getString(R.string.upload_progress, filename));
+        }
+
+        @OnClick(R.id.cancelUploadButton)
+        void onCancelUploadClick() {
+            if (uploadId == null)
+                return;
+
+            UploadService.stopUpload(uploadId);
         }
     }
 }
