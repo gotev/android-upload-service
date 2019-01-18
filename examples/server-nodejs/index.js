45e6725715881d1beadefa5b7f2ce62ea7ed45be
Mritunjay <34881570+Mritunjay10@users.noreply.github.com>
2019-01-18 23:12:18 +0530

    Update index.js

--- examples/server-nodejs/index.js
+++ examples/server-nodejs/index.js
@@ -1,169 +1,66 @@
-var express = require('express');
-var multer = require('multer');
-var fs = require('fs');
-var path = require('path');
-var os = require('os');
-var passport = require('passport');
-var HttpBasicAuth = require('passport-http').BasicStrategy;
-var app = express();
+const http = require("http");
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+const app = express();
+const expressValidator = require('express-validator');
+const expFileUpload = require("express-fileupload");
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
-var UPLOAD_PATH = "./uploads/";
-var SERVER_PORT = 3000;
+app.use(expressValidator());
+app.use(expFileUpload());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({secret: 'CSELL', saveUninitialized: false, resave: false}));
 
-var basicAuthUser = {
-    username: "test",
-    password: "pass"
-};
+app.post('/image', (req, res) => {
 
-function printRequestHeaders(req) {
-    console.log("\nReceived headers");
-    console.log("----------------");
+    const fileKeys = Object.keys(req.files);
 
-    for (var key in req.headers) {
-        console.log(key + ": " + req.headers[key]);
+    if (Object.keys(req.files).length == 0){
+
+        res.json(returnJSONData(400, 'No files', true, "Error"));
     }
 
-    console.log("");
-}
+    fileKeys.forEach((value)=>{
 
-function printRequestParameters(req) {
-    console.log("\nReceived Parameters");
-    console.log("-------------------");
+        let sampleFile = req.files[value];
+        const extension = path.extname(sampleFile.name);
 
-    for (var key in req.body) {
-        console.log(key + ": " + req.body[key]);
-    }
+        sampleFile.mv(path.dirname(require.main.filename)+'/public/uploads/users/CSELL_'
+            +randomNumber(4)+"_"+moment().valueOf()+extension, (err)=> {
 
-    if (Object.keys(req.body).length === 0)
-        console.log("no text parameters\n");
-    else
-        console.log("");
-}
+            if(err){
 
-function getEndpoints(ipAddress) {
-    return "HTTP/Multipart:              http://" + ipAddress + ":" + SERVER_PORT + "/upload/multipart\n" +
-           "HTTP/Multipart (Basic Auth): http://" + ipAddress + ":" + SERVER_PORT + "/upload/multipart-ba\n" +
-           "Binary:                      http://" + ipAddress + ":" + SERVER_PORT + "/upload/binary\n" +
-           "Binary (Basic Auth):         http://" + ipAddress + ":" + SERVER_PORT + "/upload/binary-ba\n" +
-           "401 Forbidden:               http://" + ipAddress + ":" + SERVER_PORT + "/upload/forbidden\n"
-}
-
-function printAvailableEndpoints() {
-    var ifaces = os.networkInterfaces();
-
-    Object.keys(ifaces).forEach(function (ifname) {
-        ifaces[ifname].forEach(function (iface) {
-            // skip internal (i.e. 127.0.0.1) and non-ipv4 addresses
-            if ('IPv4' !== iface.family || iface.internal !== false) {
-                return;
+                res.json(returnJSONData(200, 'File name', false, returnArray));
             }
-
-            console.log(getEndpoints(iface.address));
         });
+
+        returnArray.push(sampleFile);
     });
-}
 
-var multipartReqInterceptor = function(req, res, next) {
-    console.log("\n\nHTTP/Multipart Upload Request from: " + req.ip);
-    printRequestHeaders(req);
+    res.json(returnJSONData(200, 'File name', false, returnArray));
+});
 
-    next();
+const randomNumber = (length)=>{
+
+    let add = 1, max = 12 - add;
+
+    if ( length > max ) {
+        return generate(max) + generate(length - max);
+    }
+
+    max        = Math.pow(10, length+add);
+    let min    = max/10;
+    let number = Math.floor( Math.random() * (max - min + 1) ) + min;
+
+    return ("" + number).substring(add);
 };
 
-// configure passport for Basic Auth strategy
-passport.use('basic-admin', new HttpBasicAuth({ realm: 'Upload Service' },
-    function(username, password, done) {
-        if (username === basicAuthUser.username &&
-            password === basicAuthUser.password)  {
-            return done(null, basicAuthUser);
-        }
-        return done(null, false);
-    }
-));
+const returnJSONData = (httpCode, message, error, data) => {
 
-app.use(passport.initialize());
-var useBasicAuth = passport.authenticate('basic-admin', { session: false });
-
-// configure multer for upload management
-var fileUploadCompleted = false;
-var multerFiles = multer({
-    dest: UPLOAD_PATH,
-    rename: function (fieldname, filename) {
-        return filename;
-    },
-
-    onParseEnd: function(req, next) {
-        printRequestParameters(req);
-
-        next();
-    },
-
-    onFileUploadStart: function (file) {
-        console.log("Started file upload\n  parameter name: " +
-                    file.fieldname + "\n  file name: " +
-                    file.originalname + "\n  mime type: " + file.mimetype);
-    },
-
-    onFileUploadComplete: function (file) {
-        var fullPath = path.resolve(UPLOAD_PATH, file.originalname);
-        console.log("Completed file upload\n  parameter name: " +
-                    file.fieldname + "\n  file name: " +
-                    file.originalname + "\n  mime type: " + file.mimetype +
-                    "\n  in: " + fullPath);
-        fileUploadCompleted = true;
-    }
-});
-
-app.get('/', function(req, res) {
-    res.end("Android Upload Service Demo node.js server running!");
-});
-
-var multipartUploadHandler = function(req, res) {
-    if (fileUploadCompleted) {
-        fileUploadCompleted = false;
-        res.header('transfer-encoding', ''); // disable chunked transfer encoding
-        res.end("Upload Ok!");
-    }
+    return [{code:httpCode, message:message, error:error, value:data}];
 };
-
-var binaryUploadHandler = function(req, res) {
-    console.log("\n\nBinary Upload Request from: " + req.ip);
-    printRequestHeaders(req);
-
-    var filename = req.headers["file-name"];
-    console.log("Started binary upload of: " + filename);
-    var filepath = path.resolve(UPLOAD_PATH, filename);
-    var out = fs.createWriteStream(filepath, { flags: 'w', encoding: 'binary', fd: null, mode: '644' });
-    req.pipe(out);
-    req.on('end', function() {
-        console.log("Finished binary upload of: " + filename + "\n  in: " + filepath);
-        res.sendStatus(200);
-    });
-};
-
-// handle multipart uploads
-app.post('/upload/multipart', multipartReqInterceptor, multerFiles, multipartUploadHandler);
-app.post('/upload/multipart-ba', useBasicAuth, multipartReqInterceptor, multerFiles, multipartUploadHandler);
-
-// handle binary uploads
-app.post('/upload/binary', binaryUploadHandler);
-app.post('/upload/binary-ba', useBasicAuth,binaryUploadHandler);
-
-// endpoint which returns always 401 and a JSON response in the body
-app.post('/upload/forbidden', function(req, res) {
-    res.status(401);
-    res.json({
-        success: false,
-        message: "this endpoint always returns 401! It's for testing only"
-    });
-});
-
-var server = app.listen(SERVER_PORT, function() {
-    console.log("Web server started. Listening on all interfaces on port " +
-                server.address().port);
-
-    console.log("\nThe following endpoints are available for upload testing:\n");
-    printAvailableEndpoints();
-
-    console.log("Basic auth credentials are: " + JSON.stringify(basicAuthUser));
-});
