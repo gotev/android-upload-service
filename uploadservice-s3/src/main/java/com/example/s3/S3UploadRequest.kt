1a63235d7c8a97b159b63f62e758314f6405dc79
Borna.Fatehi <borna.fatehi@biosensics.com>
2020-11-30 19:25:50 -0500

    separate package

--- uploadservice-s3/src/main/java/com/example/s3/S3UploadRequest.kt
+++ uploadservice-s3/src/main/java/com/example/s3/S3UploadRequest.kt
@@ -1,31 +1,29 @@
 package com.example.s3
 
 import android.content.Context
-import com.amazonaws.auth.CognitoCachingCredentialsProvider
-import com.amazonaws.regions.Region
 import net.gotev.uploadservice.UploadRequest
 import net.gotev.uploadservice.UploadTask
-import java.io.File
 
-class s3UploadRequest(context: Context, serverUrl: String,
-                      credentialsProvider: CognitoCachingCredentialsProvider,
-                      region: Region,
+class S3UploadRequest(context: Context, serverUrl: String,
+                      uploadFilepath: String,
+                      serverSubpath: String,
                       bucket_name: String,
-                      bucket_key: String,
-                      Identity_key: String,
-                      file: File,
-                      ) : UploadRequest<s3UploadRequest>(context, serverUrl) {
-    protected val s3params = s3UploadTaskParameters(credentialsProvider, region, bucket_name, bucket_key, Identity_key, file);
-    
-    
+                      identityPoolId: String,
+                      region: String,
+
+                      ) : UploadRequest<S3UploadRequest>(context, serverUrl) {
+    protected val s3params = S3UploadTaskParameters(uploadFilepath, serverSubpath,
+            bucket_name , identityPoolId, region);
+
+
     override val taskClass: Class<out UploadTask>
-        get() = s3UploadTask::class.java
+        get() = S3UploadTask::class.java
 
     override fun getAdditionalParameters() = s3params.toPersistableData()
-    
+
 
     override fun startUpload(): String {
-        require(files.isNotEmpty()) { "Add at least one file to start FTP upload!" }
+        /*require(file.isNotEmpty()) { "Add at least one file to start FTP upload!" }*/
         return super.startUpload()
     }
 }
--- uploadservice-s3/src/main/java/com/example/s3/S3UploadTaskParameters.kt
+++ uploadservice-s3/src/main/java/com/example/s3/S3UploadTaskParameters.kt
@@ -1,53 +1,55 @@
 package com.example.s3
 
 import android.os.Parcelable
-import com.amazonaws.auth.CognitoCachingCredentialsProvider
-import com.amazonaws.regions.Region
+import com.amazonaws.regions.Regions
 import kotlinx.android.parcel.Parcelize
 import net.gotev.uploadservice.persistence.Persistable
 import net.gotev.uploadservice.persistence.PersistableData
 import java.io.File
 
 @Parcelize
-data class s3UploadTaskParameters (
-        var credentialsProvider: CognitoCachingCredentialsProvider,
-        var region: Region,
-        var bucket_name: String = "",
-        var bucket_key: String = "com.aws.s3.BUCKET_NAME",
-        var Identity_key: String = "com.aws.s3.IDENTITY_POOL_ID",
-        var file : File
-
+data class S3UploadTaskParameters (
+        var uploadFilepath : String,
+        var serverSubpath: String = "",
+        var bucket_name: String,
+        var identityPoolId: String,
+        var region: String = Regions.US_EAST_1.name,
+        var bucket_key: String = "com.aws.s3$bucket_name",
+        var Identity_key: String = "com.aws.s3$identityPoolId",
 ) : Parcelable, Persistable {
-    companion object : Persistable.Creator<s3UploadTaskParameters> {
+    companion object : Persistable.Creator<S3UploadTaskParameters> {
         /**
          * The default FTP connection timeout in milliseconds.
          */
 
         private object CodingKeys {
-            const val credentialsProvider = "credentialsProvider"
-            const val region = "region"
+            const val uploadFilepath = "uploadFilepath"
+            const val serverSubpath = "serverSubpath"
             const val bucket_name = "bucket_name"
+            const val identityPoolId = "identityPoolId"
+            const val region = "region"
             const val bucket_key = "bucket_key"
             const val Identity_key = "Identity_key"
-            const val file = "file"
         }
 
-        override fun createFromPersistableData(data: PersistableData) = s3UploadTaskParameters(
-                credentialsProvider = data.getCognitoCachingCredentialsProvider(CodingKeys.credentialsProvider),
-                region = data.getRegion(CodingKeys.region),
+        override fun createFromPersistableData(data: PersistableData) = S3UploadTaskParameters(
+                uploadFilepath = data.getString(CodingKeys.uploadFilepath),
+                serverSubpath = data.getString(CodingKeys.serverSubpath),
                 bucket_name = data.getString(CodingKeys.bucket_name),
+                identityPoolId = data.getString(CodingKeys.identityPoolId),
+                region = data.getString(CodingKeys.region),
                 bucket_key = data.getString(CodingKeys.bucket_key),
                 Identity_key = data.getString(CodingKeys.Identity_key),
-                file = data.getFile(CodingKeys.file)
         )
     }
 
     override fun toPersistableData() = PersistableData().apply {
-        putCognitoCachingCredentialsProvider(CodingKeys.credentialsProvider, credentialsProvider)
-        putRegion(CodingKeys.region, region)
+        putString(CodingKeys.uploadFilepath, uploadFilepath)
+        putString(CodingKeys.serverSubpath, serverSubpath)
         putString(CodingKeys.bucket_name, bucket_name)
+        putString(CodingKeys.identityPoolId, identityPoolId)
+        putString(CodingKeys.region, region)
         putString(CodingKeys.bucket_key, bucket_key)
         putString(CodingKeys.Identity_key, Identity_key)
-        putFile(CodingKeys.file, file)
     }
 }
