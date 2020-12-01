a4ea7a31396a4d1f6f3a09fa92dd84b153ba7802
Borna.Fatehi <borna.fatehi@biosensics.com>
2020-11-30 19:15:47 -0500

    cleaningup parameter variables

--- examples/app/uploadservice-s3/src/main/java/com/example/uploadservice/s3/S3UploadRequest.kt
+++ examples/app/uploadservice-s3/src/main/java/com/example/uploadservice/s3/S3UploadRequest.kt
@@ -3,27 +3,27 @@ package com.example.uploadservice.s3
 import android.content.Context
 import net.gotev.uploadservice.UploadRequest
 import net.gotev.uploadservice.UploadTask
-import java.io.File
 
-class s3UploadRequest(context: Context, serverUrl: String,
+class S3UploadRequest(context: Context, serverUrl: String,
+                      uploadFilepath: String,
+                      serverSubpath: String,
+                      bucket_name: String,
                       identityPoolId: String,
                       region: String,
-                      bucket_name: String,
-                      bucket_key: String,
-                      Identity_key: String,
-                      file: File,
-) : UploadRequest<s3UploadRequest>(context, serverUrl) {
-    protected val s3params = s3UploadTaskParameters(identityPoolId, region, bucket_name, bucket_key, Identity_key, file);
+
+) : UploadRequest<S3UploadRequest>(context, serverUrl) {
+    protected val s3params = S3UploadTaskParameters(uploadFilepath, serverSubpath,
+            bucket_name , identityPoolId, region);
 
 
     override val taskClass: Class<out UploadTask>
-        get() = s3UploadTask::class.java
+        get() = S3UploadTask::class.java
 
     override fun getAdditionalParameters() = s3params.toPersistableData()
 
 
     override fun startUpload(): String {
-        require(files.isNotEmpty()) { "Add at least one file to start FTP upload!" }
+        /*require(file.isNotEmpty()) { "Add at least one file to start FTP upload!" }*/
         return super.startUpload()
     }
 }
--- examples/app/uploadservice-s3/src/main/java/com/example/uploadservice/s3/S3UploadTask.kt
+++ examples/app/uploadservice-s3/src/main/java/com/example/uploadservice/s3/S3UploadTask.kt
@@ -1,7 +1,9 @@
 package com.example.uploadservice.s3
 
+import android.util.Log
 import com.amazonaws.auth.CognitoCachingCredentialsProvider
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferNetworkLossHandler
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferState
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
 import com.amazonaws.regions.Region
@@ -10,11 +12,12 @@ import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.CannedAccessControlList
 import net.gotev.uploadservice.UploadTask
 import net.gotev.uploadservice.network.HttpStack
+import java.io.File
 
-class s3UploadTask : UploadTask() {
+class S3UploadTask : UploadTask() {
 
     private val s3params by lazy {
-        s3UploadTaskParameters.createFromPersistableData(params.additionalParameters)
+        S3UploadTaskParameters.createFromPersistableData(params.additionalParameters)
     }
 
     override fun upload(httpStack: HttpStack) {
@@ -23,31 +26,34 @@ class s3UploadTask : UploadTask() {
 
         val s3 = AmazonS3Client(credentialsProvider, Region.getRegion(Regions.fromName(s3params.region)))
         val transferUtility = TransferUtility.builder().s3Client(s3).context(context).build()
-
+        val file = File(s3params.uploadFilepath)
 
         val observer = transferUtility.upload(
                 s3params.bucket_name,
-                params.serverUrl,
-                s3params.file,
+                s3params.serverSubpath,
+                file,
                 CannedAccessControlList.Private
         )
+        TransferNetworkLossHandler.getInstance(context)
 
         observer.setTransferListener(object : TransferListener {
             override fun onStateChanged(id: Int, state: TransferState) {
                 if (state == TransferState.COMPLETED) {
-
+                    Log.i("UtilityObserver","Upload Finished!");
                 } else if (state == TransferState.FAILED) {
-
+                    Log.i("UtilityObserver","Upload Failed!");
                 } else {
-
+                    Log.i("UtilityObserver","Unkown!");
                 }
             }
 
             override fun onProgressChanged(id: Int, bytesCurrent: Long, bytesTotal: Long) {
                 val progress = bytesCurrent.toDouble() / bytesTotal * 100
+                Log.i("UtilityObserver","Progress: " + progress);
             }
 
             override fun onError(id: Int, ex: Exception) {
+                Log.e("UtilityObserver","Error Occured");
             }
         })
     }
