15e457fd5fa3d8f68389cd39a6e344a46b609e8a
Alex Gotev <alex@gotev.net>
2021-06-05 10:40:55 +0200

    feat: updated SimpleMultipartUpload to Kotlin 1.4.32. Stopped using jcenter. Removed kotlin android extensions. Updated all dependencies.

--- examples/SimpleMultipartUpload/app/src/main/java/it/gotev/testapp/MainActivity.kt
+++ examples/SimpleMultipartUpload/app/src/main/java/it/gotev/testapp/MainActivity.kt
@@ -3,8 +3,8 @@ package it.gotev.testapp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import net.gotev.uploadservice.protocols.multipart.MultipartUploadRequest
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        uploadButton.setOnClickListener {
+        findViewById<Button>(R.id.uploadButton).setOnClickListener {
             pickFile()
         }
     }
