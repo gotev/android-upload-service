7d6bddcc77f9b2c8bc65d64bbb783323dca7ccee
Alex Gotev <alex@gotev.net>
2023-07-09 15:23:48 +0200

    feat: updated SimpleMultipartUpload demo app

--- examples/SimpleMultipartUpload/app/src/main/java/it/gotev/testapp/MainActivity.kt
+++ examples/SimpleMultipartUpload/app/src/main/java/it/gotev/testapp/MainActivity.kt
@@ -1,10 +1,15 @@
 package it.gotev.testapp
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import net.gotev.uploadservice.protocols.multipart.MultipartUploadRequest
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +20,20 @@ class MainActivity : AppCompatActivity() {
         const val pickFileRequestCode = 42
     }
 
+    private val notificationPermissionRequest = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        // custom logic when the user either allows or disallows notifications
+    }
+
+    private fun checkPostNotificationsPermission() {
+        if (Build.VERSION.SDK_INT >= 33 && ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            notificationPermissionRequest.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPostNotificationsPermission()
 
         findViewById<Button>(R.id.uploadButton).setOnClickListener {
             pickFile()
